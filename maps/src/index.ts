78977f3b9802b3de4f7f57e50fe4cd7880f6a7b5
Saarthak Gupta <saarthakgupta08@gmail.com>
2021-08-04 23:59:17 +0530

    complete maps project

--- maps/src/index.ts
+++ maps/src/index.ts
@@ -1,1 +1,13 @@
-console.log("Hi there!");
+import { User } from './User';
+import { Company } from './Company';
+import { CustomMap } from './CustomMap';
+
+const map = new CustomMap('map');
+const user = new User();
+console.log(user);
+
+const company = new Company();
+console.log(company);
+
+map.addMarker(user);
+map.addMarker(company);
