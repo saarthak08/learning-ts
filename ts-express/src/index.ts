9595e05a4a3e15a54808c623315ad6ac7e14a66a
Saarthak Gupta <saarthakgupta08@gmail.com>
2021-08-08 14:34:45 +0530

    add ts-express code

--- ts-express/src/index.ts
+++ ts-express/src/index.ts
@@ -1,12 +1,50 @@
-import express from "express";
-import * as dotenv from "dotenv";
+import express, { Request, Response, NextFunction } from "express";
+import dotenv from "dotenv";
+import cors from "cors";
+import helmet from "helmet";
+import morgan from "morgan";
 
+dotenv.config();
+
+const PORT: number = parseInt(process.env["PORT"] as string);
 const app = express();
+
+app.use(morgan("dev"));
+app.use(helmet());
+app.use(cors());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 
 app.get("/", (_, res) => {
 	res.send("Well done!");
 });
 
-app.listen(3000, () => {
-	console.log("The application is listening on port 3000!");
+app.listen(() => {
+	console.log(`Listening on port ${PORT}`);
 });
+
+// development error handler
+// will print stacktrace
+if (process.env["NODE_ENV"] === "development") {
+	app.use(function (
+		err: Error,
+		req: Request,
+		res: Response,
+		next: NextFunction
+	) {
+		console.log(err);
+
+		res.status(500).send("Internal Server Error");
+	});
+} else {
+	// production error handler
+	// no stacktraces leaked to user
+	app.use(function (
+		err: Error,
+		req: Request,
+		res: Response,
+		next: NextFunction
+	) {
+		res.status(500).send("Internal Server Error");
+	});
+}
